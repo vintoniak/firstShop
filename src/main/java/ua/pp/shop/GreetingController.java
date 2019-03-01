4ebadb588a0f18a20892f9b8a8833c8d43296169
Volodymyr Vintoniak <vintoniak7@gmail.com>
2019-03-01 22:44:08 +0200

    add filter

--- src/main/java/ua/pp/shop/GreetingController.java
+++ src/main/java/ua/pp/shop/GreetingController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import ua.pp.shop.domain.Message;
 import ua.pp.shop.repos.MessageRepo;
 
+import java.util.List;
 import java.util.Map;
 
 @Controller
@@ -45,16 +46,29 @@ public class GreetingController {
     public String add(@RequestParam String type, @RequestParam String name,
                       @RequestParam String productNumber,@RequestParam Integer numberOfGoods,
                       @RequestParam Double unitPrice,@RequestParam Double totalPrice, Map<String,Object> model){
-        Message message = new Message(type, name, productNumber, numberOfGoods, unitPrice, totalPrice);
-        message.setType(type);
-        message.setName(name);
-        message.setProductNumber(productNumber);
-        message.setNumberOfGoods(numberOfGoods);
-        message.setUnitPrice(unitPrice);
-        message.setTotalPrice(totalPrice);
-        messageRepo.save(message);
-        model.put("message", message);
+        Message messages = new Message(type, name, productNumber, numberOfGoods, unitPrice, totalPrice);
+        messages.setType(type);
+        messages.setName(name);
+        messages.setProductNumber(productNumber);
+        messages.setNumberOfGoods(numberOfGoods);
+        messages.setUnitPrice(unitPrice);
+        messages.setTotalPrice(totalPrice);
+        messageRepo.save(messages);
+        model.put("message", messages);
         return "main";
     }
 
+    @PostMapping("filter")
+    public String filter(@RequestParam String type, Map<String,Object> model){
+        Iterable<Message> messages;
+        if(type != null && !type.isEmpty()){
+             messages = messageRepo.findByType(type);
+        } else{
+            messages = messageRepo.findAll();
+        }
+
+
+        model.put("message", messages);
+        return "main";
+    }
 }
--- src/main/java/ua/pp/shop/repos/MessageRepo.java
+++ src/main/java/ua/pp/shop/repos/MessageRepo.java
@@ -3,5 +3,8 @@ package ua.pp.shop.repos;
 import org.springframework.data.repository.CrudRepository;
 import ua.pp.shop.domain.Message;
 
+import java.util.List;
+
 public interface MessageRepo extends CrudRepository<Message, Integer> {
+   List<Message> findByType(String type);
 }
